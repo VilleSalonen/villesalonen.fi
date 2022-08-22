fda74dc5d5948097b48a3a2acab642de08259773
Ville Salonen <ville.salonen@iki.fi>
2022-08-22 16:52:21 +0300

    Fix formatting for azurerm_static_site resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,9 +26,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_static_site" "villesalonen" {
-  name                     = "ville-salonen"
+  name                = "ville-salonen"
   resource_group_name = azurerm_resource_group.rg.name
-  location                 = "westeurope"
+  location            = "westeurope"
 }
 
 resource "azurerm_dns_zone" "villesalonen" {
