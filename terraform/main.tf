f7e50f2471bde284f9e0bc70e5d67bd0c74a4e10
Ville Salonen <ville.salonen@iki.fi>
2022-08-22 15:34:14 +0300

    Initial terraform configuration
    Only includes resource group for now.

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate9692"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "static-apps"
+  location = "swedencentral"
+}
